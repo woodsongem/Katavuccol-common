f33bd890486e0b319dfe673055f3e707a006f634
woodsongem <jess.sajan@gmail.com>
2019-09-13 17:22:21 -0400

    base

---
+++ src/KatavuccolCommon/Extensions/KatavuccolExtensions.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KatavuccolCommon.Extensions
+{
+    public static class KatavuccolExtensions
+    {
+        public static bool AnyWithNullCheck<TSource>(this IEnumerable<TSource> value)
+        {
+            if (value == null)
+                return false;
+            return value.Any();
+        }
+
+        public static bool IsEmpty(this object value)
+        {
+            return value == null;
+        }
+
+        public static bool IsEmpty(this string value)
+        {
+            if (value == null)
+                return true;
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static long ToLong(this string value)
+        {
+            if (value.IsEmpty())
+                return 0;
+            if (long.TryParse(value, out long result))
+                return 0;
+            return result;
+        }
+    }
+}
